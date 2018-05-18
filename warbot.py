76ccb4f0ad2a437ccb780dd315e85bd33c6d55ba
Kyle Moffett <kyle.moffett@hotmail.com>
2018-05-17 19:48:10 -0700

    re-added load/unload commands

--- warbot.py
+++ warbot.py
@@ -19,6 +19,22 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.command(hidden=True)
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command(hidden=True)
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
+
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
